ad9ad69fb5a889457ab8f1fc38faa3e0a2227db6
Syrine Sassi <47872045+SyrineSassi@users.noreply.github.com>
2022-11-07 14:16:48 +0100

    Create StockServiceMock.java

---
+++ src/main/java/tn/esprit/rh/achat/test/StockServiceMock.java
@@ -0,0 +1,59 @@
+package tn.esprit.rh.achat.test;
+
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tn.esprit.rh.achat.entities.Stock;
+import tn.esprit.rh.achat.repositories.StockRepository;
+import tn.esprit.rh.achat.services.StockServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+
+
+@ExtendWith(MockitoExtension.class)
+ class StockServiceMock {
+
+    @InjectMocks
+    StockServiceImpl stockService;
+
+
+    @Mock
+    StockRepository stockRepository;
+
+    Stock s = new Stock((long)1,"testLibelle1",10,150);
+    Stock s1 = new Stock((long)2,"testLibelle1",50,100);
+    Stock s2 = new Stock((long)3,"testLibelle2",5,80);
+    List<Stock> stock = new ArrayList<Stock>() {{add(s1); add(s2);}};
+
+
+    
+
+    @Test
+    public void addStock() {
+        Mockito.when(stockRepository.save(Mockito.any(Stock.class))).thenReturn(s);
+        assertNotNull(stockService.addStock(s));
+        //verify(stockRepository).save(s);
+    }
+
+    @Test
+    public void deleteStock() {
+        stockService.deleteStock((long)3);
+        verify(stockRepository).deleteById((long)3);
+    }
+
+    @Test
+    public void updateStock() {
+        Mockito.when(stockRepository.save(Mockito.any(Stock.class))).thenReturn(s);
+        s.setQte(55);
+        assertNotNull(stockService.updateStock(s));
+        assertEquals(Optional.of(55), s.getQte());
+    }
+}
